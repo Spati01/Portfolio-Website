7f83c2aab3c0dc1029e11f16df9f1043f0e08621
subhadip <subhadippati30@gmail.com>
2024-08-08 12:14:58 +0530

    cmd

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 /*===================== Typed JS =======================*/
 const typed = new Typed('.multiple-text',{
-  strings:['Web Application Developer<🧑‍💻>','Android  Developer<💻>','Java Developer<🖥️>'],
+  strings:['Web Application Developer<🧑‍💻>','Java Developer<🖥️>'],
   typeSpeed:100,
   backSpeed:100,
   backDelay:1000,
